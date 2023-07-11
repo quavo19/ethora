ba2e0dbb1d65eda41268f0a34d7c5c138193e27f
Quavo <quavo@Newtons-MacBook-Pro.local>
2023-07-11 02:18:27 +0000

    add story view

--- client-reactnative/src/components/RoomList/RoomListItemStories.tsx
+++ client-reactnative/src/components/RoomList/RoomListItemStories.tsx
@@ -2,165 +2,63 @@ import Close from '../../assets/close'
 //import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, SafeAreaView, Pressable, View, Image, Text, Dimensions, StatusBar, ScrollView, Platform } from 'react-native';
-import Stories from 'rn-story';
+import ExpoInstaStory from 'expo-insta-story';
 const { width } = Dimensions.get('window');
 
 export default function RoomListItemStory() {
-  const [data, setData] = useState<Data[]>([
+  const data = [
     {
-      profileImage:
-        'https://shorturl.at/fhUV1',
-      profileName: 'Abdullah Ansari',
-      viewed: false,
       id: 1,
+      avatar_image:
+        'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+      user_name: 'Muhammad Bilal',
       stories: [
         {
-          media: 'https://shorturl.at/mpwQ1',
-          mediaType: 'image',
-          seeMoreUrl: 'https://abdullahansari.me'
+          story_id: 1,
+          story:
+            'https://image.freepik.com/free-vector/universe-mobile-wallpaper-with-planets_79603-600.jpg',
+          swipeText: 'Custom swipe text for this story',
+          onPress: () => console.log('story 1 swiped'),
+          duration: 10, //This tells the duration of each screen
         },
         {
-          media: 'https://shorturl.at/jpJ58',
-          mediaType: 'image',
-          duration: 12000
+          story_id: 2,
+          story:
+            'https://image.freepik.com/free-vector/mobile-wallpaper-with-fluid-shapes_79603-601.jpg',
+          duration: 10,
         },
-        {
-          media: 'https://shorturl.at/ckvyT',
-          mediaType: 'image',
-        },
-      ]
+      ],
     },
     {
-      profileImage:
-        'https://shorturl.at/fhUV1',
-      profileName: 'Abdullah Ansari',
-      viewed: false,
       id: 2,
+      avatar_image:
+        'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      user_name: 'Test User',
       stories: [
         {
-          media: 'https://shorturl.at/jpJ58',
-          mediaType: 'image',
-          duration: 12000
+          story_id: 1,
+          story:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU',
+          swipeText: 'Custom swipe text for this story',
+          onPress: () => console.log('story 1 swiped'),
+          duration: 10,
         },
         {
-          media: 'https://shorturl.at/mpwQ1',
-          mediaType: 'image',
-          seeMoreUrl: 'https://abdullahansari.me'
+          story_id: 2,
+          story: 'https://demo-link/123-123-123.mp4',
+          swipeText: 'Custom swipe text for this story',
+          onPress: () => console.log('story 2 swiped'),
+          duration: 10,
+          /* This indicates that this item is a video,
+             when passing a video urlm this field must be added
+          */
+          isVideo: true,
         },
-        {
-          media: 'https://shorturl.at/ckvyT',
-          mediaType: 'image',
-        },
-      ]
+      ],
     },
-    
-    
-  ]);
-  //setting this state to null will close the story view
-  const [currentStoryIndex, setCurrentStoryIndex] = useState<number | null>(null);
-
-  useEffect(() => {
-    //we are adding header to each story item
-    const _tempData = [...data];
-    _tempData.forEach((story) => {
-      story.stories.forEach((storyItem) => {
-        storyItem.header = <View style={[styles.avatarAndIconsContainer]}>
-          {/* THE AVATAR AND USERNAME  */}
-          <View style={[styles.avatarAndIconsContainer]}>
-            {/* <LinearGradient
-              colors={[`rgba(0,0,0,0.25)`, 'transparent']}
-              style={[styles.linearGradient]}
-            /> */}
-            <View style={styles.avatarAndProfileContainer}>
-              <Image
-                style={[styles.profileImage]}
-                source={{
-                  uri: story?.profileImage
-                }} />
-              <View>
-                <Text
-                  numberOfLines={1}
-                  style={[{ width: width / 1.75 }, styles.profileName]}>
-                  {story?.profileName}
-                </Text>
-              </View>
-            </View>
-            {/* END OF THE AVATAR AND USERNAME */}
-          </View>
-          <View style={styles.iconContainer}>
-            {/* THE CLOSE BUTTON */}
-            <Pressable style={{ marginLeft: 12 }} onPress={() => setCurrentStoryIndex(null)}>
-              <Close height={28} width={28} fill={"#fff"} stroke={"#fff"} />
-            </Pressable>
-            {/* END OF CLOSE BUTTON */}
-          </View>
-        </View>
-      });
-    });
-    setData(_tempData);
-  }, []);
-
-  const handleNext = () => {
-    console.log("went to next")
-  };
-
-  const handlePrevious = () => {
-    console.log("went to prev")
-  };
-  
+  ];
   return (
-    <SafeAreaView 
-     style={{
-      position: "absolute",
-      zIndex: 20,
-      top: 70,
-     }}
-    >
-      <StatusBar />
-      {/* you can also use FlatList here */}
-      <ScrollView horizontal>
-        {data.map((story, index) => (
-          <Pressable
-            key={"story-" + story.id}
-            onPress={() => setCurrentStoryIndex(index)}
-            style={[styles.storyContainer]}>
-            <View
-              style={[styles.imageContainer,
-              story.viewed ? styles.viewedStory : styles.newStory]}>
-              <Image
-                style={[styles.storyImage]}
-                resizeMode={"cover"}
-                source={{ uri: story?.profileImage }}
-              />
-            </View>
-            <Text
-              numberOfLines={1}
-              style={[styles.profileNameHorizontal]}>
-              {story?.profileName}
-            </Text>
-          </Pressable>
-        ))}
-      </ScrollView>
-      {currentStoryIndex !== null &&
-        <Stories
-          stories={data[currentStoryIndex].stories}
-          //called when user taps on next
-          onNext={() => handlePrevious()}
-          //called when user taps on previous
-          onPrevious={() => handleNext()}
-          // close story view if there are no more stories to go next to
-          onAllStoriesEnd={() => setCurrentStoryIndex(null)}
-          //close story view if there are no more stories to go back to
-          onPreviousFirstStory={() => setCurrentStoryIndex(null)}
-          //custom loading component
-          loadingComponent={<Text style={{
-            position: 'absolute',
-            color: '#fff',
-            bottom: Dimensions.get("window").height / 2,
-            left: Dimensions.get("window").width / 2, transform: [{ translateX: -50 }]
-        }}>Custom Loading...</Text>}
-        />}
-    </SafeAreaView>
+    <ExpoInstaStory data={data} duration={10} />
   );
 }
 
