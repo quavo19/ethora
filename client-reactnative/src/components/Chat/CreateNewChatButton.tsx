7fcee40ad0fff9e63cdb53302c60eadb892b22b9
Akite Donald <donaldakite27@gmail.com>
2023-07-23 13:31:50 +0000

    Lower create button

--- client-reactnative/src/components/Chat/CreateNewChatButton.tsx
+++ client-reactnative/src/components/Chat/CreateNewChatButton.tsx
@@ -13,7 +13,13 @@ export const CreateNewChatButton: React.FC<ICreateNewChatButton> = ({
   onPress,
 }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={{padding: 10, paddingLeft: 15, minHeight:hp("15%"), width:wp("100%")}}>
+    <View style={{
+      position: "absolute",
+      top: 780,
+      left: 350,
+      zIndex: 10,
+    }}>
+      <TouchableOpacity onPress={onPress} style={{padding: 10, paddingLeft: 15, minHeight:hp("9%"), width:wp("100%")}}>
       <HStack alignItems={'center'}>
         <Box
           w={hp('5.5%')}
@@ -25,27 +31,8 @@ export const CreateNewChatButton: React.FC<ICreateNewChatButton> = ({
           marginRight={2}>
           <AntDesign name="plus" color={'#FFF'} size={hp('4.3%')} />
         </Box>
-        <View>
-          <Text
-            fontSize={hp('2%')}
-            fontFamily={textStyles.boldFont}
-            _dark={{
-              color: 'warmGray.50',
-            }}
-            color="coolGray.800">
-            Create a new room
-          </Text>
-          <Text
-            fontFamily={textStyles.regularFont}
-            fontSize={hp('1.5%')}
-            color="coolGray.600"
-            _dark={{
-              color: 'warmGray.100',
-            }}>
-            Your own room, share with anyone you like
-          </Text>
-        </View>
       </HStack>
     </TouchableOpacity>
+    </View>
   );
 };
