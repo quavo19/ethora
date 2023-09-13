eb0163cb2c31fcb0d8a9f5352e437b1068209130
Akite Donald <donaldakite27@gmail.com>
2023-09-13 15:17:10 +0000

    Edit the width of createchat button

--- client-reactnative/src/components/Chat/CreateNewChatButton.tsx
+++ client-reactnative/src/components/Chat/CreateNewChatButton.tsx
@@ -3,7 +3,6 @@ import {Box, HStack, Text, View} from 'native-base';
 import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen';
 import {textStyles} from '../../../docs/config';
 import {Image, TouchableOpacity} from 'react-native';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export interface ICreateNewChatButton {
   onPress: () => void;
@@ -15,18 +14,17 @@ export const CreateNewChatButton: React.FC<ICreateNewChatButton> = ({
   return (
     <View style={{
       position: "absolute",
-      bottom: 80,
+      top: 30,
       zIndex: 10,
-      right: 0,
+      left: 0,
       width: 90,
       
     }}>
-      <TouchableOpacity onPress={onPress} style={{padding: 10, paddingLeft: 15, minHeight:hp("9%"), width:wp("100%")}}>
+      <TouchableOpacity onPress={onPress} style={{padding: 5, paddingLeft: 15}}>
       <HStack>
         <Box
           w={hp('5.5%')}
           h={hp('5.5%')}
-          
           rounded="full"
           justifyContent={'center'}
           alignItems="center"
@@ -34,7 +32,7 @@ export const CreateNewChatButton: React.FC<ICreateNewChatButton> = ({
           <Image 
               style={{
               height: 100,
-              width: "100%",
+              width: 55,
             }}
             source={require('../../assets/create.png')} />                
         </Box>
